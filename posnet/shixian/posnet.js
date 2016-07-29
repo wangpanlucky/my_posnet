0d8da1d052ac16d1fe53e8111c34cdf37a3ab628
wanpanlucky <1462607050@qq.com>
2016-07-29 13:56:39 +0800

    posnet

---
+++ posnet/shixian/posnet.js
@@ -0,0 +1,172 @@
+/**
+ * Created by lucky on 16-7-28.
+ */
+
+
+//条形码->邮编
+/*global module*/
+/**
+ * @return {boolean}
+ */
+function IsLegal(barCodes) {
+    let result = Array.from(barCodes).filter(function (code) {
+        return code === ' ' || code === '|' || code === ':';
+    });
+    return result.length === barCodes.length;
+}
+
+function isHaveBarFrame(barCodes) {
+    let m = barCodes.length;
+    let firstFrame = barCodes.slice(0, 2);
+    let endFrame = barCodes.slice(m - 2, m);
+    return !!(firstFrame[0] === '|' && firstFrame[1] === ' ' && endFrame[0] === ' ' && endFrame[1] === '|');
+}
+
+function getCodeList() {
+    return ['||:::', ':::||', '::|:|', '::||:', ':|::|', ':|:|:', ':||::', '|:::|', '|::|:', '|:|::'];
+}
+
+function deleteFrame(barCodes) {
+    return barCodes.substr(2, barCodes.length - 4);
+}
+
+function formatBarCodes(barcodes) {
+    return barcodes.split(' ');
+}
+
+function filterSpace(barCodes) {
+    return barCodes.filter(function (codes) {
+        return codes.length === 5;
+    });
+}
+
+function matchZipCodes(codeList, formatCodes) {
+    let allCodes = formatCodes.map(function (code) {
+        return codeList.indexOf(code);
+    });
+    return allCodes.reduce(function (pre, cur) {
+        return pre.toString().concat(cur.toString());
+    });
+}
+
+function checkCd(allCodes) {
+    let array = allCodes.split('');
+    let result = array.reduce(function (pre, cur) {
+        return parseInt(pre) + parseInt(cur);
+    });
+    return result % 10 === 0;
+}
+
+function deleteCheckDigit(allCodes) {
+    return allCodes.substr(0, allCodes.length - 1);
+}
+
+function finalFormat(zipCodes) {
+    if (zipCodes.length == 9) {
+        return zipCodes.substr(0, zipCodes.length - 4).concat('-').concat(zipCodes.substr(5, zipCodes.length - 5));
+    }
+    else
+        return zipCodes;
+}
+
+function covertToZipCodes(barCodes) {
+    if (!IsLegal(barCodes) && !isHaveBarFrame(barCodes)) {
+        return undefined;
+    }
+    let codeList = getCodeList();
+    let modifiedBarCodes = deleteFrame(barCodes);
+    let formatCodes= formatBarCodes(modifiedBarCodes);
+    let noSpaceCodes=filterSpace(formatCodes);
+    let allCodes = matchZipCodes(codeList, noSpaceCodes);
+    let zipCodes;
+    if (!checkCd(allCodes)) {
+        return undefined;
+    } else {
+        zipCodes = deleteCheckDigit(allCodes);
+        return finalFormat(zipCodes);
+    }
+}
+
+//邮编->条形码
+function checkBit(zipCodes) {
+    return zipCodes.length === 5 || zipCodes.length === 9 || zipCodes.length === 10;
+}
+
+function getCodeLists() {
+    return ['||:::', ':::||', '::|:|', '::||:', ':|::|', ':|:|:', ':||::', '|:::|', '|::|:', '|:|::'];
+}
+
+function formatCodes(zipCodes) {
+    let modifiedCodes = zipCodes.split('-');
+    return modifiedCodes.reduce(function (pre, cur) {
+        return pre.toString().concat(cur.toString());
+    })
+}
+
+function calculateCd(modifiedCodes) {
+    let array = modifiedCodes.split('');
+    let result = array.reduce(function (pre, cur) {
+        return parseInt(pre) + parseInt(cur);
+    });
+    return result % 10 === 0 ? (result % 10).toString() : (10 - result % 10).toString();
+}
+
+function connectCodes(modifiedCodes, cd) {
+    return modifiedCodes.concat(cd);
+}
+
+function matchBarCodes(newZipCodes, codeList) {
+    let codes = newZipCodes.split('');
+    let newZipCode = codes.map(function (code) {
+        let code1 = parseInt(code);
+        return codeList.find(function (code, index) {
+            if (code1 === index) {
+                return code;
+            }
+        })
+    });
+    return newZipCode.reduce(function (pre, cur) {
+        return pre.concat(cur);
+    });
+}
+
+function addBarFrame(newBarCodes) {
+    return '| '.concat(newBarCodes).concat(' |');
+}
+
+function covertToBarcode(zipCodes) {
+    let isTrue = checkBit(zipCodes);
+    if (!isTrue) {
+        return undefined;
+    }
+    let codeList = getCodeLists();
+    let modifiedCodes = formatCodes(zipCodes);
+    let cd = calculateCd(modifiedCodes);
+    let newZipCodes = connectCodes(modifiedCodes, cd);
+    let newBarCodes = matchBarCodes(newZipCodes, codeList);
+    return addBarFrame(newBarCodes);
+
+}
+
+
+
+module.exports = {IsLegal: IsLegal,
+    isHaveBarFrame: isHaveBarFrame,
+    getCodeList: getCodeList,
+    deleteFrame: deleteFrame,
+    formatBarCodes: formatBarCodes,
+    filterSpace:filterSpace,
+    matchZipCodes: matchZipCodes,
+    checkCd: checkCd,
+    deleteCheckDigit: deleteCheckDigit,
+    finalFormat: finalFormat,
+    covertToZipCodes: covertToZipCodes,
+    checkBit: checkBit,
+    getCodeLists: getCodeLists,
+    formatCodes: formatCodes,
+    calculateCd: calculateCd,
+    connectCodes: connectCodes,
+    matchBarCodes:matchBarCodes,
+    addBarFrame: addBarFrame,
+    covertToBarcode:covertToBarcode
+};
---
+++ posnet/spec/shixian/posnet_spec.js
@@ -0,0 +1,195 @@
+/**
+ * Created by lucky on 16-7-28.
+ */
+/*global require*/
+const obj = require("../../shixian/posnet.js");
+
+//条形码->邮编
+describe("IsLegal", function () {
+    it("judge the barCodes is legal", function () {
+        let barCodes = "| :|::|:|:|:||:::: |";
+        let result = obj.IsLegal(barCodes);
+        expect(result).toEqual(true);
+    });
+});
+
+
+describe ("isHaveBarFrame",function () {
+  it("is have bar frame",function () {
+    let barCodes="| :|::|:|:|:||:::: |";
+    let result=obj.isHaveBarFrame(barCodes);
+    expect(result).toEqual(true);
+  });
+});
+
+describe("IsLegal", function () {
+    it("judge the barCodes is legal", function () {
+        let barCodes = "|:|::|:|:|:||:*:::|";
+        let result = obj.IsLegal(barCodes);
+        expect(result).toEqual(false);
+    });
+});
+
+describe("getCodeList", function () {
+    it("get number's barcode", function () {
+        let expected = ['||:::', ':::||', '::|:|', '::||:', ':|::|', ':|:|:', ':||::', '|:::|', '|::|:', '|:|::'];
+        expect(obj.getCodeList()).toEqual(expected);
+    });
+});
+
+describe("deleteFrame", function () {
+    it("delete bar frame ", function () {
+        let barCodes = "| ::|:| ::||: |";
+        let expected = "::|:| ::||:";
+        let result = obj.deleteFrame(barCodes);
+        expect(result).toEqual(expected);
+    });
+});
+
+describe("formatBarCodes", function () {
+    it("split with ' '", function () {
+        let barCodes = "::|:| ::||:";
+        let expected = ["::|:|", "::||:"];
+        let result = obj.formatBarCodes(barCodes);
+        expect(result).toEqual(expected);
+    });
+});
+
+describe("filterSpace", function () {
+    it("split with ' '", function () {
+        let barCodes = ["::|:|"," ","::||:"];
+        let expected = ["::|:|","::||:"];
+        let result;
+        result = obj.filterSpace(barCodes);
+        expect(result).toEqual(expected);
+    });
+});
+
+describe("matchZipCodes", function () {
+    it("match with codeList", function () {
+        let codeList = ['||:::', ':::||', '::|:|', '::||:', ':|::|', ':|:|:', ':||::', '|:::|', '|::|:', '|:|::'];
+        let forMateCodes = ['::|:|', '::||:', ':|::|', ':|:|:', ':||::'];
+        let expected = '23456';
+        let result = obj.matchZipCodes(codeList, forMateCodes);
+        expect(result).toEqual(expected);
+    });
+});
+
+describe("checkCd", function () {
+    it("check digit", function () {
+        let codes = '23456';
+        let result = obj.checkCd(codes);
+        expect(result).toEqual(true);
+    });
+});
+
+describe("deleteCheckDigit", function () {
+    it("get zipPost ", function () {
+        let allCodes = '23456';
+        let expected = '2345';
+        let result = obj.deleteCheckDigit(allCodes);
+        expect(result).toEqual(expected);
+    });
+});
+
+describe("finalFormat",function () {
+    it("finally format",function () {
+        let zipCodes="123456789";
+        let expected="12345-6789";
+        let result=obj.finalFormat(zipCodes);
+        expect(result).toEqual(expected);
+    });
+});
+
+describe("covertToZipCodes", function () {
+    it("get final zipCodes", function () {
+        let barCodes = "| ::|:|  ::||:  :|::|  :|:|:   :||:: |";
+        let zipCodes = '2345';
+        let result = obj.covertToZipCodes(barCodes);
+        expect(result).toEqual(zipCodes);
+    });
+    it("should return undefined if barcode is invalid", function () {
+        let barCodes = "| ::|:| ::||: :|::| :|:|: :||::invalid char |";
+        let result = obj.covertToZipCodes(barCodes);
+        expect(result).toBeUndefined();
+    });
+});
+
+//邮编->条形码
+describe("checkBit", function () {
+    it("check bit", function () {
+        let zipCodes = '23456';
+        let result = obj.checkBit(zipCodes);
+        expect(result).toEqual(true);
+    });
+});
+
+describe("getCodeLists", function () {
+    it("get number's barcode", function () {
+        let expected = ['||:::', ':::||', '::|:|', '::||:', ':|::|', ':|:|:', ':||::', '|:::|', '|::|:', '|:|::'];
+        expect(obj.getCodeLists()).toEqual(expected);
+    });
+});
+
+describe("formatCodes", function () {
+    it("get number", function () {
+        let zipCodes = '2345-6';
+        let expected = '23456';
+        let result = obj.formatCodes(zipCodes);
+        expect(result).toEqual(expected);
+    });
+});
+
+describe("calculateCd", function () {
+    it("get checkDigit", function () {
+        let modifiedCodes = '23456';
+        let expected = '0';
+        let result = obj.calculateCd(modifiedCodes);
+        expect(result).toEqual(expected);
+    });
+});
+
+
+describe("connectCodes", function () {
+    it("connect codes and cd", function () {
+        let modifiedCodes = '23456';
+        let cd = '0';
+        let expected = '234560';
+        let result = obj.connectCodes(modifiedCodes, cd);
+        expect(result).toEqual(expected);
+    });
+});
+
+
+describe("matchBarCodes", function () {
+    it("match zipCodes with code list", function () {
+        let codeList = ['||:::', ':::||', '::|:|', '::||:', ':|::|', ':|:|:', ':||::', '|:::|', '|::|:', '|:|::'];
+        let newZipCodes = '234560';
+        let expected = "::|:|::||::|::|:|:|::||::||:::";
+        let result = obj.matchBarCodes(newZipCodes, codeList);
+        expect(result).toEqual(expected);
+    });
+});
+
+describe("addBarFrame", function () {
+    it("add bar frame", function () {
+        let newBarCodes = "::|:|::||::|::|:|:|::||::||:::";
+        let expected = "| ::|:|::||::|::|:|:|::||::||::: |";
+        let result = obj.addBarFrame(newBarCodes);
+        expect(result).toEqual(expected);
+    });
+});
+
+describe("covertToBarcode", function () {
+    it("get final zipCodes", function () {
+        let zipCodes = '23456';
+        let barCodes = "| ::|:|::||::|::|:|:|::||::||::: |";
+        let result = obj.covertToBarcode(zipCodes);
+        expect(result).toEqual(barCodes);
+    });
+    it("should return undefined if zipCode's bit is wrong", function () {
+        let zipCodes = "23";
+        let result = obj.covertToBarcode(zipCodes);
+        expect(result).toBeUndefined();
+    });
+});
